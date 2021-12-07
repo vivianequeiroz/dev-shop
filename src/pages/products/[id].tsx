cede1267a896401e7fbb3ae2b585cb8286bd1602
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-07 17:16:06 -0300

    chore: footer component usage in products page

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next';
 
 import { useRouter } from 'next/dist/client/router';
 
+import { Footer } from '../../components/Footer';
+
 const Product: NextPage = () => {
   const router = useRouter();
   const productId = router.query.id as string;
@@ -9,6 +11,7 @@ const Product: NextPage = () => {
   return (
     <div>
       <h1>Product: {productId}</h1>
+      <Footer />
     </div>
   );
 };
