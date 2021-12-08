6c649105066bc8f40515649ae0f393c2886a1fff
João Bispo <joaobispo2077@gmail.com>
2021-12-08 20:22:22 -0300

    refactor: load product id from server side

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,16 +1,25 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 
-import { useRouter } from 'next/dist/client/router';
+type ProductProps = {
+  id: string;
+};
 
-const Product: NextPage = () => {
-  const router = useRouter();
-  const productId = router.query.id as string;
-
+const Product: NextPage<ProductProps> = ({ id }) => {
   return (
     <div>
-      <h1>Product: {productId}</h1>
+      <h1>Product: {id}</h1>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.params?.id;
+
+  return {
+    props: {
+      id,
+    },
+  };
 };
 
 export default Product;
