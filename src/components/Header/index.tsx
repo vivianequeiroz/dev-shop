43e13e8061eb35d9220f6e0f4250bfcee6187a10
João Bispo <joaobispo2077@gmail.com>
2021-12-08 20:10:45 -0300

    feat: add Header structure with behavior

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,38 +1,84 @@
-import { Flex, HStack, Icon, Link } from '@chakra-ui/react';
-import { GetServerSideProps } from 'next';
-import { getSession } from 'next-auth/react';
-import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
+import { useState } from 'react';
+
+import {
+  Center,
+  Divider,
+  Flex,
+  IconButton,
+  Stack,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import { signOut, useSession } from 'next-auth/react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { RiLogoutBoxRLine } from 'react-icons/ri';
 
 import { Logo } from '../Logo';
 import { User } from '../User';
+import { NavBar } from '../NavBar';
 
 export function Header() {
+  const [isLoading, setLoading] = useState(false);
+
+  const { status } = useSession();
+  const isUserAuthenticated = status === 'authenticated';
+
+  const isDesktopScreen = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+  const dividerOrientation = isDesktopScreen ? 'vertical' : 'horizontal';
+
+  const handleUserLogOut = async () => {
+    setLoading(true);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      await signOut();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <Flex as="header">
+    <Flex
+      as="header"
+      direction={['column', 'row']}
+      width="100%"
+      align="center"
+      justify="space-between"
+    >
       <Logo />
-      <HStack>
-        <Link>Screens</Link>
-        <Link>Keyboards</Link>
-        <Link>Mouses</Link>
-        <Link>Chairs</Link>
-      </HStack>
-      <HStack>
-        <Icon
-          as={AiOutlineSearch}
-          role="img"
-          width={60}
-          height={60}
-          fill="white"
-        />
+      <NavBar />
+      <Stack spacing={[2, 4]} direction="row" align="center">
         <User />
-        <Icon
-          as={AiOutlineShoppingCart}
-          role="img"
-          width={60}
-          height={60}
-          fill="white"
+        <Center height="4rem" width="1rem">
+          <Divider orientation={dividerOrientation} color="black" />
+        </Center>
+
+        <IconButton
+          colorScheme="green"
+          icon={<AiOutlineShoppingCart size={24} />}
+          aria-label="Log out"
         />
-      </HStack>
+
+        {isUserAuthenticated && (
+          <>
+            <Center height="4rem" width="1rem">
+              <Divider orientation={dividerOrientation} color="black" />
+            </Center>
+            <IconButton
+              as="button"
+              colorScheme="red"
+              icon={<RiLogoutBoxRLine />}
+              aria-label="Log out"
+              isLoading={isLoading}
+              onClick={handleUserLogOut}
+              type="button"
+            />
+          </>
+        )}
+      </Stack>
     </Flex>
   );
 }
