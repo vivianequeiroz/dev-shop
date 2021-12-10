b58149a63e43549e04220554f97fe6fa0c5657fe
João Bispo <joaobispo2077@gmail.com>
2021-12-09 22:08:25 -0300

    feat: add components that show cart item

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,15 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import 'react-toastify/dist/ReactToastify.css';
 
 import type { AppProps } from 'next/app';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
-import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <ChakraProvider>
         <Component {...pageProps} />
-        <ToastContainer
-          position="top-right"
-          autoClose={8000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          draggable={false}
-          pauseOnHover
-          closeOnClick
-        />
       </ChakraProvider>
     </SessionProvider>
   );
