6483f43569eeceeeb90ebbb01394513431c0d118
João Bispo <joaobispo2077@gmail.com>
2021-12-09 22:06:01 -0300

    feat: add modal with cart items

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import {
   Center,
@@ -7,6 +7,16 @@ import {
   IconButton,
   Stack,
   useBreakpointValue,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  Heading,
 } from '@chakra-ui/react';
 import { signOut, useSession } from 'next-auth/react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
@@ -15,8 +25,13 @@ import { RiLogoutBoxRLine } from 'react-icons/ri';
 import { Logo } from '../Logo';
 import { User } from '../User';
 import { NavBar } from '../NavBar';
-
+import { CartItem } from '../CartItem';
+import { getProducts } from '../../services/productsServices';
+import products from '../../services/productsServices/products.json';
 export function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = useRef();
+
   const [isLoading, setLoading] = useState(false);
 
   const { status } = useSession();
@@ -60,7 +75,8 @@ export function Header() {
         <IconButton
           colorScheme="green"
           icon={<AiOutlineShoppingCart size={24} />}
-          aria-label="Log out"
+          aria-label="Open cart"
+          onClick={onOpen}
         />
 
         {isUserAuthenticated && (
@@ -80,6 +96,49 @@ export function Header() {
           </>
         )}
       </Stack>
+
+      <Modal finalFocusRef={finalRef.current} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent minWidth={['full', '500px', '600px']}>
+          <ModalHeader>
+            {' '}
+            <Heading fontSize="2xl" fontWeight="extrabold" color="gray.600">
+              Carrinho de compras (3 items)
+            </Heading>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Stack spacing={{ base: '8', md: '10' }} flex="2">
+              <Stack spacing="6">
+                {products.map((product) => (
+                  <CartItem
+                    key={product.id}
+                    imageUrl={product.images[0]}
+                    currency="BRL"
+                    name={product.title}
+                    {...product}
+                    quantity={6}
+                  />
+                ))}
+              </Stack>
+            </Stack>
+          </ModalBody>
+
+          <ModalFooter
+            align="center"
+            justifyContent="space-between"
+            px={3}
+            flexWrap="wrap"
+          >
+            <Button variant="ghost" color="green.400" onClick={onClose}>
+              Continuar comprando
+            </Button>
+            <Button colorScheme="green" onClick={onClose}>
+              Comprar tudo
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
