9fc69f70cd7672dda25a3b9f6b5449e742030b9a
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-12-10 21:30:07 -0300

    feat: add of scroll to product section when clicking on it through navbar

--- src/components/Home/ProductsExhibition/index.tsx
+++ src/components/Home/ProductsExhibition/index.tsx
@@ -152,7 +152,7 @@ export function ProductsExhibition() {
 
       <Divider mt="5rem" mb="5rem" />
 
-      <Heading d="flex" as="h1" justifyContent="flex-start" mt="2rem">
+      <Heading d="flex" as="h1" justifyContent="flex-start" mt="2rem" id="monitors">
         Monitores
       </Heading>
       <Wrap spacing="30px" marginTop="5">
@@ -270,7 +270,7 @@ export function ProductsExhibition() {
 
       <Divider mt="5rem" mb="5rem" />
 
-      <Heading d="flex" as="h1" justifyContent="flex-start" mt="2rem">
+      <Heading d="flex" as="h1" justifyContent="flex-start" mt="2rem" id="keyboards">
         Teclados
       </Heading>
       <Wrap spacing="30px" marginTop="5">
@@ -397,7 +397,7 @@ export function ProductsExhibition() {
 
       <Divider mt="5rem" mb="5rem" />
 
-      <Heading d="flex" as="h1" justifyContent="flex-start" mt="2rem">
+      <Heading d="flex" as="h1" justifyContent="flex-start" mt="2rem" id="mouses">
         Mouses
       </Heading>
       <Wrap spacing="30px" marginTop="5">
@@ -515,7 +515,7 @@ export function ProductsExhibition() {
 
       <Divider mt="5rem" mb="5rem" />
 
-      <Heading d="flex" as="h1" justifyContent="flex-start" mt="2rem">
+      <Heading d="flex" as="h1" justifyContent="flex-start" mt="2rem" id="chairs">
         Cadeiras
       </Heading>
       <Wrap spacing="30px" marginTop="5">
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -4,22 +4,22 @@ import { Flex, Link, SimpleGrid } from '@chakra-ui/react';
 
 export const NavBar: React.FC = () => {
   return (
-    <Flex as="nav" width="100%" justify="center" padding={2}>
+    <Flex as="nav" width="100%" justify="center" padding={2} >
       <SimpleGrid
         templateColumns={{ base: '1fr 1fr', md: '1fr 1fr 1fr 1fr' }}
         templateRows={{ base: '1fr 1fr', md: '1fr' }}
         gap={[2, 4, 8, 10]}
       >
-        <Link fontSize="1.3rem" fontWeight="bold" color="blue.700">
+        <Link fontSize="1.3rem" fontWeight="bold" color="blue.700" href="#monitors">
           Monitores
         </Link>
-        <Link fontSize="1.3rem" fontWeight="bold" color="blue.700">
+        <Link fontSize="1.3rem" fontWeight="bold" color="blue.700" href="#keyboards" >
           Teclados
         </Link>
-        <Link fontSize="1.3rem" fontWeight="bold" color="blue.700">
+        <Link fontSize="1.3rem" fontWeight="bold" color="blue.700" href="#mouses">
           Mouses
         </Link>
-        <Link fontSize="1.3rem" fontWeight="bold" color="blue.700">
+        <Link fontSize="1.3rem" fontWeight="bold" color="blue.700" href="#chairs">
           Cadeiras
         </Link>
       </SimpleGrid>
